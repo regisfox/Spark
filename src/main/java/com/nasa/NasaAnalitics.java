fcc1e3073f238903b96123885362a4b57b101272
reginaldofagundes <reginaldo.s.fagundes@gmail.com>
2018-05-23 19:35:20 -0300

    correções

--- src/main/java/com/nasa/NasaAnalitics.java
+++ src/main/java/com/nasa/NasaAnalitics.java
@@ -1,44 +1,65 @@
 package com.nasa;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 
 public class NasaAnalitics {
 
+	private static String error404 = "404 -";
 	private static Logger logger = Logger.getLogger(NasaAnalitics.class);
 
 	public static void main(String[] args) {
-
+		// Caminho do arquivo para leitura.
 		String inputFile = "C:\\Users\\rsfagundes\\Downloads\\NASA_access_log_Aug95\\NASA_access_log_Aug95";
+		// String inputFile = args[0];
 
 		SparkConf conf = new SparkConf().setAppName("nasa-analitics").setMaster("local[2]").set("spark.executor.memory", "1g");
 
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> file = sc.textFile(inputFile);
+
+		JavaRDD<String> totalErrors404 = file.filter(object -> String.valueOf(object).contains(error404));
 		
-		JavaRDD<String> hostUnicos = file.distinct().filter(object -> String.valueOf(object).contains("404 -"));
-		JavaRDD<String> totalError404 = file.filter(object -> String.valueOf(object).contains("404 -"));
+		List<String> dadosHost = file.collect();
+		Set<String> hostUnicos = new HashSet<>();
+		for (String linha : dadosHost) {
+			String[] arrayValores = linha.split(" ");
+			hostUnicos.add(arrayValores[0]);
+		}
 		
-		JavaRDD<String> mais5UrlsError404 = file.distinct().filter(new Function<String, Boolean>() {
-			@Override
-			public Boolean call(String v1) throws Exception {
-				return v1.contains("404 - ");
+		List<String> dadosDia = file.collect();
+		Set<String> dias = new HashSet<>();
+		for (String linha : dadosDia) {
+			String[] arrayValores = linha.split(" ");
+			if (arrayValores.length > 3) {
+				String data = arrayValores[3].substring(1, 13);
+				dias.add(data);
 			}
+		}
+	
+		JavaDoubleRDD bytes = file.mapToDouble(t -> {
+			String[] arrayValores = t.split(" ");
+			double s = 0;
+			if (StringUtils.isNumeric(arrayValores[(arrayValores.length-1)])) {	
+				s = Double.parseDouble(arrayValores[(arrayValores.length-1)]);
+			}
+			return s+s;
 		});
-		JavaRDD<String> pythonLines = file.filter(line -> line.contains("Python"));
-		JavaRDD<String> totaBytes = file.filter(object -> String.valueOf(object).contains("404 -"));
+		java.math.BigDecimal totalBytes = new java.math.BigDecimal(bytes.sum()) ;
 		
-		long numHostUnicos = hostUnicos.count();
-		long numTotalErrors404 = totalError404.count();
-		long numMais5UrlsError404 = mais5UrlsError404.count();
-		long numTotaBytes = totaBytes.count();
-		logger.info("Número de hosts únicos: " + numHostUnicos);
-		logger.info("O total de erros 404: " + numTotalErrors404);
-		logger.info("Os 5 URLs que mais causaram erro 404: " + numMais5UrlsError404);
-		logger.info("O total de bytes retornados: " + numTotaBytes);
+		logger.info("O total de erros 404: " + totalErrors404.count());
+		logger.info("Os 5 URLs que mais causaram erro 404: " + 0);
+		logger.info("Quantidade de erros 404 por dia: " + 0);
+		logger.info("O total de bytes retornados: " + totalBytes.toString());
+		logger.info("Número de hosts únicos: " + hostUnicos.size());
 
 	}
 }
